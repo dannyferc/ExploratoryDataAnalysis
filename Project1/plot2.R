653f9d056b7c8b5ff968f3f2fd2832c511301551
dannyferc <dannyferc@gmail.com>
2014-10-20 13:23:18 +0200

    Update plot2.R

--- Project1/plot2.R
+++ Project1/plot2.R
@@ -6,7 +6,7 @@ source("LoadSubset.R")
 data <- LoadSubset()
 
 #create png file
-png("plot2.png", width = 504, height = 504)
+png("plot2.png", width = 480, height = 480)
 
 #add the plot to the file
 plot(data$Time, data$Global_active_power, type ="l", xlab = "", ylab ="Global Active Power (Kilowatts)")
